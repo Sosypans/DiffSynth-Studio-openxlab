0b631d52cdbbb90bb3fde86d9da0f231a4b1adf8
TinsleyME <93901876+TinsleyME@users.noreply.github.com>
2024-05-07 14:15:36 +0800

    Update ModelDownload.py

--- ModelDownload.py
+++ ModelDownload.py
@@ -1,12 +1,12 @@
 import os
 
 def sd_model_download():
-  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//realisticasianthaila_v20 -d /home/xlab-app-center/models/stable-diffusion -o realisticasianthaila_v20.ckpt")
-  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//DreamShaper_8_pruned.safetensors -d /home/xlab-app-center/models/stable-diffusion -o DreamShaper_8_pruned.safetensors")
-  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//rev-animated-v1-2-2.safetensors -d /home/xlab-app-center/models/stable-diffusion -o rev-animated-v1-2-2.safetensors")
+  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//realisticasianthaila_v20 -d /home/xlab-app-center/models/stable_diffusion -o realisticasianthaila_v20.ckpt")
+  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//DreamShaper_8_pruned.safetensors -d /home/xlab-app-center/models/stable_diffusion -o DreamShaper_8_pruned.safetensors")
+  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//rev-animated-v1-2-2.safetensors -d /home/xlab-app-center/models/stable_diffusion -o rev-animated-v1-2-2.safetensors")
 
 def sdXL_model_download():
-  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//realistic_bailingXL_v2.safetensors -d /home/xlab-app-center/models/stable-diffusion_xl -o realistic_bailingXL_v2.safetensors")
+  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//realistic_bailingXL_v2.safetensors -d /home/xlab-app-center/models/stable_diffusion_xl -o realistic_bailingXL_v2.safetensors")
 
 def AnimateDiff_model_download():
   os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/houshaowei/AnimateDiff/weight//mm_sd_v15.ckpt -d /home/xlab-app-center/models/AnimateDiff -o mm_sd_v15.ckpt")
