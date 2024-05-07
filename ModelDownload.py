f552789be655674d36496ee255dbdbec86b0371f
TinsleyME <93901876+TinsleyME@users.noreply.github.com>
2024-05-07 09:40:44 +0800

    Update ModelDownload.py

--- ModelDownload.py
+++ ModelDownload.py
@@ -5,6 +5,10 @@ def sd_model_download():
   os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//DreamShaper_8_pruned -d /home/xlab-app-center/models/Stable-diffusion -o DreamShaper_8_pruned.safetensors")
   os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/Tinsley/realisticasianthaila/weight//rev-animated-v1-2-2 -d /home/xlab-app-center/models/Stable-diffusion -o rev-animated-v1-2-2.safetensors")
 
+def AnimateDiff_model_download():
+  os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://download.openxlab.org.cn/models/houshaowei/AnimateDiff/weight//mm_sd_v15.ckpt -d /home/xlab-app-center/models/AnimateDiff -o mm_sd_v15.ckpt")
+
 #多进程启动
 import multiprocessing
 multiprocessing.Process(target=sd_model_download).start()
+multiprocessing.Process(target=AnimateDiff_model_download).start()
