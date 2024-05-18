ac085845fa434901fc4ec48c433c3a4d04e72e09
Sosypans <58300651+Sosypans@users.noreply.github.com>
2024-05-18 13:03:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ st.set_page_config(layout="wide")
 import torch
 torch.cuda.set_per_process_memory_fraction(0.999, 0)
 
+git add app.py 
+git commit -m "Add application file"
+git push
 
 st.markdown("""
 # DiffSynth Studio
